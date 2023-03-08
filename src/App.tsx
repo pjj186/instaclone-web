9558340ed401a0c57bd97d73522ee3c8a311daab
JongJae Park <pjj186@naver.com>
2023-03-08 20:10:17 +0900

    Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  const isLoggedIn = false;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
