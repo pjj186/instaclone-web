df57051e357ca0f318fe0762a69848f9a1e20537
JongJae Park <pjj186@naver.com>
2023-04-17 01:45:25 +0900

    React Hook Form

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,17 +1,23 @@
 import {
   faFacebookSquare,
   faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styled from "styled-components";
-import AuthLayout from "../components/auth/AuthLayout";
-import BottomBox from "../components/auth/BottomBox";
-import Button from "../components/auth/Button";
-import FormBox from "../components/auth/FormBox";
-import Input from "../components/auth/Input";
-import Separator from "../components/auth/Separator";
-import PageTitle from "../components/PageTitle";
-import routes from "../routes";
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import AuthLayout from '../components/auth/AuthLayout';
+import BottomBox from '../components/auth/BottomBox';
+import Button from '../components/auth/Button';
+import FormBox from '../components/auth/FormBox';
+import Input from '../components/auth/Input';
+import Separator from '../components/auth/Separator';
+import PageTitle from '../components/PageTitle';
+import routes from '../routes';
+import { useForm } from 'react-hook-form';
+
+interface FormData {
+  username: string;
+  password: string;
+}
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -22,6 +28,16 @@ const FacebookLogin = styled.div`
 `;
 
 const Login = () => {
+  const { register, watch, handleSubmit } = useForm<FormData>();
+
+  const onSubmitValid = (data: FormData) => {
+    console.log(data);
+  };
+
+  const onSubmitInvalid = (data: any) => {
+    console.log(data, 'invalid');
+  };
+
   return (
     <AuthLayout>
       <PageTitle title="Login" />
@@ -29,9 +45,22 @@ const Login = () => {
         <div>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
         </div>
-        <form>
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInvalid)}>
+          <Input
+            {...register('username', {
+              required: 'Username is required',
+              minLength: 5,
+            })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register('password', {
+              required: 'Password is required',
+            })}
+            type="password"
+            placeholder="Password"
+          />
           <Button type="submit" value="Log in" />
         </form>
         <Separator />
