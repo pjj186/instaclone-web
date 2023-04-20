cbc88604419b3910244ca13b63b097b5c88a8f32
JongJae Park <pjj186@naver.com>
2023-04-20 15:26:14 +0900

    Photo Component part One

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,58 @@
 import { useHistory } from 'react-router-dom';
-import { logUserOut } from '../apollo';
+import { gql, useQuery } from '@apollo/client';
+import styled from 'styled-components';
+import Avatar from '../components/Avatar';
+import { FatText } from '../components/shared';
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      id
+      user {
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+    }
+  }
+`;
+
+const PhotoContainer = styled.div`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.borderColor};
+  margin-bottom: 20px;
+`;
+
+const PhotoHeader = styled.div`
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+  color: black;
+`;
+
+const Username = styled(FatText)`
+  margin-left: 5px;
+`;
 
 const Home = () => {
+  const { data } = useQuery(FEED_QUERY);
+
   const histroy = useHistory();
   return (
     <div>
-      <h1>Welcome we did it!</h1>
-      <button onClick={() => logUserOut(histroy)}>Log out now!</button>
+      {data?.seeFeed?.map((photo: any) => (
+        <PhotoContainer key={photo.id}>
+          <PhotoHeader>
+            <Avatar url={photo.user.avatar} />
+            <Username>{photo.user.username}</Username>
+          </PhotoHeader>
+        </PhotoContainer>
+      ))}
     </div>
   );
 };
