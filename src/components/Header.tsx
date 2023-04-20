7ac204efa77cbba76e7d2bcbf23174ab9a726694
JongJae Park <pjj186@naver.com>
2023-04-20 12:19:35 +0900

    Header part Two

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { useReactiveVar } from '@apollo/client';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faCompass, faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
+import { isLoggedInVar } from '../apollo';
+import { Link } from 'react-router-dom';
+import routes from '../routes';
+import useUser from '../hooks/useUser';
 
 const SHeader = styled.header`
   width: 100%;
@@ -29,7 +34,17 @@ const Icon = styled.span`
   margin-left: 15px;
 `;
 
+const Button = styled.span`
+  background-color: ${(props) => props.theme.accent};
+  border-radius: 4px;
+  padding: 3px 15px;
+  color: white;
+  font-weight: 600;
+`;
+
 const Header = () => {
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const loggedInUser = useUser();
   return (
     <SHeader>
       <Wrapper>
@@ -37,15 +52,23 @@ const Header = () => {
           <FontAwesomeIcon icon={faInstagram} size="2x" />
         </Column>
         <Column>
-          <Icon>
-            <FontAwesomeIcon icon={faHome} size="lg" />
-          </Icon>
-          <Icon>
-            <FontAwesomeIcon icon={faCompass} size="lg" />
-          </Icon>
-          <Icon>
-            <FontAwesomeIcon icon={faUser} size="lg" />
-          </Icon>
+          {isLoggedIn ? (
+            <>
+              <Icon>
+                <FontAwesomeIcon icon={faHome} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faCompass} size="lg" />
+              </Icon>
+              <Icon>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </Icon>
+            </>
+          ) : (
+            <Link to={routes.home}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </Column>
       </Wrapper>
     </SHeader>
