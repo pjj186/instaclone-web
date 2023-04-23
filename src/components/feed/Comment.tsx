4b22b2c43e5a7704e81dee912693a389f907f5ad
JongJae Park <pjj186@naver.com>
2023-04-23 23:06:04 +0900

    Parsing Hashtags

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FatText } from '../shared';
 import styled from 'styled-components';
+import sanitizeHtml from 'sanitize-html';
 
 interface ICommentProps {
   author: string;
@@ -12,13 +13,31 @@ const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
   color: black;
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
 const Comment = (props: ICommentProps) => {
+  const cleanedPayload = sanitizeHtml(
+    props.payload.replace(/#[\w]+/g, '<mark>$&</mark>'),
+    {
+      allowedTags: ['mark'],
+    },
+  );
   return (
     <CommentContainer>
       <FatText>{props.author}</FatText>
-      <CommentCaption>{props.payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanedPayload,
+        }}
+      />
     </CommentContainer>
   );
 };
